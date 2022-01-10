799958504572a116443d3b2c8e3f6744a9f32688
gitball66 <sivakorn.ing@gmail.com>
2022-01-11 01:01:23 +0700

    update

--- 2.js
+++ 2.js
@@ -1,18 +1,24 @@
-console.log("Enter Num Subject");
+console.log("Enter Score");
 let stdin3 = process.openStdin();
 stdin3.addListener("data", function (str3) {
-  let nummajor = parseInt(str3);
-  for (i = 0; i < nummajor; i++) {
-    if (i === i) {
-      console.log("subject" + (i + 1));
-      let stdin4 = process.openStdin();
-      stdin4.addListener("data", function (str4) {
-        console.log(str4);
-        stdin4.destroy();
-      });
-    }
-    if (i === (nummajor-1)) {
-      console.log("END");
-    }
+  let score = parseInt(str3);
+  if (score < 50) {
+    console.log("Your Grade : F");
   }
+  if (score >= 50 && score < 60) {
+    console.log("Your Grade : D");
+  }
+  if (score >= 60 && score < 70) {
+    console.log("Your Grade : C");
+  }
+  if (score >= 70 && score < 80) {
+    console.log("Your Grade : b");
+  }
+  if (score >= 80) {
+    console.log("Your Grade : A");
+  }
+  if (score == "") {
+    console.log("Input Score");
+  }
+  stdin3.destroy();
 });
